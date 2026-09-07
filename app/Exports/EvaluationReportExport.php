4040b14822a31e48b93d534c8245cb8c8011ad61
TranSongKy <nguyencharles501@gmail.com>
2026-09-07 10:55:10 +0700

    auto align#4

--- app/Exports/EvaluationReportExport.php
+++ app/Exports/EvaluationReportExport.php
@@ -7,8 +7,13 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
 
-class EvaluationReportExport implements FromCollection, WithHeadings, WithMapping, WithTitle
+class EvaluationReportExport implements FromCollection, WithHeadings, WithMapping, WithTitle, WithStyles, WithColumnWidths
 {
     public function __construct(protected Collection $summary, protected string $academicYear)
     {
@@ -43,5 +48,27 @@ class EvaluationReportExport implements FromCollection, WithHeadings, WithMappin
     public function title(): string
     {
         return 'Báo cáo Hội đồng '.$this->academicYear;
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        $lastRow = $sheet->getHighestRow();
+
+        $sheet->getStyle("A1:G{$lastRow}")
+            ->getBorders()->getAllBorders()
+            ->setBorderStyle(Border::BORDER_THIN);
+
+        $sheet->getStyle('A1:G1')->getFont()->setBold(true);
+        $sheet->getStyle('A1:G1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('A2:A'.$lastRow)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('C2:F'.$lastRow)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('G2:G'.$lastRow)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+        return [];
+    }
+
+    public function columnWidths(): array
+    {
+        return ['A' => 8, 'B' => 26, 'C' => 14, 'D' => 16, 'E' => 16, 'F' => 18, 'G' => 24];
     }
 }
